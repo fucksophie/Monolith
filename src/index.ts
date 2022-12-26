2b98976214a98950c3e61b10d5f175eee97dc299
yourfriend <friend@yourfriend.lv>
2022-12-26 03:17:07 +0200

    more cool shit (needs fragmenting)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ConnInfo, serve } from "https://deno.land/std@0.170.0/http/server.ts";
 import { exists } from "./deps.ts";
 import { Database } from "./libs/Database.ts";
 import { ServerList } from "./libs/ServerList.ts";
+import { User } from "./libs/User.ts";
 import { login, register } from "./routes/auth.ts";
 import { heartbeat, servers } from "./routes/heartbeat.ts";
 
@@ -24,26 +25,73 @@ async function handler(req: Request, conn: ConnInfo): Promise<Response> {
         return servers(req);
     }
 
+    if(path.pathname == "/api/skinUpdate" && req.method == "POST") {
+        let authenicated: User|undefined
+
+        if(req.headers.has("cookie")) {
+            const session = req.headers.get("cookie")?.replace("session=", "")
+            if(session) authenicated = DB.getBySession(session!);
+        }
+
+        if(authenicated) {
+            const data = await req.arrayBuffer();
+            if(data.byteLength > 5e+6) {
+                return new Response("", {status: 413})
+            }
+
+            Deno.writeFileSync("./skins/"+authenicated.username+".png", new Uint8Array(data));
+            return new Response(`done`)
+        } else {
+            return new Response("", {status: 403})
+        }
+    }
+    if(path.pathname.startsWith("/skin/")) {
+        if(await exists("./skins/"+path.pathname.substring(6))) {
+            return new Response(await Deno.readFile("./skins/"+path.pathname.substring(6)), {
+                headers:{"Content-Type":"image/png"}
+            })
+        } else {
+            return new Response(await Deno.readFile("./skins/!!missing.png"), {
+                headers:{"Content-Type":"image/png"}
+            })
+        }
+    }
+
+    if(path.pathname == "/api/whoami") {
+        let authenicated: User|undefined
+
+        if(req.headers.has("cookie")) {
+            const session = req.headers.get("cookie")?.replace("session=", "")
+            if(session) authenicated = DB.getBySession(session!);
+        }
+
+        if(authenicated) {
+            return new Response(`{"username":"${authenicated.username}","sessions":${authenicated.openSessions.length}}`)
+        } else {
+            return new Response("", {status: 403})
+        }
+    }
     if(path.pathname == "/") {
         return new Response(Deno.readTextFileSync("website/index.html"), {headers:{"Content-Type":"text/html"}})
     }
 
     const location = path.pathname.substring(1);
 
-    if(await exists("website/" + location)) return passOn(location)
-    if(await exists("website/" + location+".html")) return passOn(location+".html")
+    if(await exists("website/" + location)) return await passOn(location)
+    if(await exists("website/" + location+".html")) return await passOn(location+".html")
 
     return new Response("404 Not Found", {status: 404})
 }
 
-function passOn(location: string) {
+async function passOn(location: string) {
     let contentType = "text/html"
 
     if(location.endsWith(".css")) contentType = "text/css"
     if(location.endsWith(".js")) contentType = "text/javascript"
     if(location.endsWith(".png")) contentType = "image/png"
+    if(location.endsWith(".gif")) contentType = "image/gif"
 
-    return new Response(Deno.readTextFileSync("website/"+location), {
+    return new Response(await Deno.readFile("website/"+location), {
         headers:{"Content-Type":contentType}
     })
 }
